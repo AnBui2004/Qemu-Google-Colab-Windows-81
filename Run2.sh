97cd2f2cc39122430ce3fb99e6a0e0eca8d473d6
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-05-27 12:34:09 +0700

    Create Run2.sh

---
+++ Run2.sh
@@ -0,0 +1,23 @@
+sudo apt-get update && apt-get install qemu -y
+sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
+sudo apt install qemu-system-x86 -y
+qemu-img create -f raw 8.img 64G
+wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
+wget -O 8.iso 'https://www.dropbox.com/s/qkl9md34406oplb/Windows_8.1_U3_x86_Lite.iso?dl=1'
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 8G \
+  -cpu EPYC \
+  -boot order=d \
+  -drive file=8.iso,media=cdrom \
+  -drive file=8.img,format=raw \
+  -drive file=RTL8139F.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -cpu core2duo \
+  -smp cores=2 \
+  -device rtl8139,netdev=n0 -netdev user,id=n0 \
+  -accel tcg,thread=multi \
